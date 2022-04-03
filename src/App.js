6bb6748982a137e12f17388cff5ecdf9e9ce3d2e
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2022-04-03 11:56:56 +0530

    Chore: Configuring app and index

--- src/App.js
+++ src/App.js
@@ -1,7 +1,108 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./Screens/Homepage/Homepage";
+import Movies from "./Screens/Movies/Movies";
+import Liked from "./Screens/Liked/Liked";
+import WatchLater from "./Screens/WatchLater/WatchLater";
+import History from "./Screens/History/History";
+import Playlists from "./Screens/Playlists/Playlists";
+import Playlist from "./Screens/Playlist/Playlist";
+import Authenticate from "./Screens/Authenticate/Authenticate";
+import Mockman from "mockman-js";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/mock" element={<Mockman />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar /> <Homepage />{" "}
+            </>
+          }
+        />
+        <Route
+          path="/movies"
+          element={
+            <>
+              <Navbar /> <Movies categoryPage={false} />{" "}
+            </>
+          }
+        />
+        <Route
+          path="/movies/:categoryName"
+          element={
+            <>
+              <Navbar /> <Movies categoryPage={true} />{" "}
+            </>
+          }
+        />
+        <Route
+          path="/liked"
+          element={
+            <>
+              <Navbar /> <Liked />{" "}
+            </>
+          }
+        />
+        <Route
+          path="/watch-later"
+          element={
+            <>
+              <Navbar /> <WatchLater />{" "}
+            </>
+          }
+        />
+        <Route
+          path="/history"
+          element={
+            <>
+              <Navbar /> <History />{" "}
+            </>
+          }
+        />
+        <Route
+          path="/playlists"
+          element={
+            <>
+              <Navbar /> <Playlists />{" "}
+            </>
+          }
+        />
+        <Route
+          path="/playlist/:playlistId"
+          element={
+            <>
+              <Navbar /> <Playlist />{" "}
+            </>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Authenticate
+              cardTitle="LOGIN"
+              checkboxLabel="Remember me"
+              alternate="Create New Account"
+            />
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <Authenticate
+              cardTitle="SIGN UP"
+              checkboxLabel="I agree to the terms and conditions"
+              alternate="Login With Existing Account"
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ProductsProvider } from "./Context/products-context.js";
+import { VideosProvider } from "./Context/videos-context.js";
+import { ToastProvider } from "./Context/toast-context.js";
+import { NavbarProvider } from "./Context/navbar-context.js";
+import { TokenProvider } from "./Context/token-context.js";
+import { makeServer } from "./server";
+
+makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProductsProvider>
+      <VideosProvider>
+        <ToastProvider>
+          <NavbarProvider>
+            <TokenProvider>
+              <App />
+            </TokenProvider>
+          </NavbarProvider>
+        </ToastProvider>
+      </VideosProvider>
+    </ProductsProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
